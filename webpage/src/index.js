698b787b834377d883e45c365651b8c470521f08
alavas <savala.justin@gmail.com>
2019-03-19 00:19:26 -0400

    GS-104 Replaced default favicon.

--- webpage/src/index.js
+++ webpage/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import dotenv from 'dotenv'
-import * as serviceWorker from './serviceWorker'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 dotenv.config()
@@ -14,5 +13,3 @@ ReactDOM.render(
 	</Router>,
 	document.getElementById('root')
 )
-
-serviceWorker.unregister()
